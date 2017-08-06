96c157347b90b764cc14e5a91a552cdb3b7fef8b
Daren Desjardins <darend@gmail.com>
2017-08-05 21:18:30 -0700

    Check out to local dir

--- bin/circleci.sh
+++ bin/circleci.sh
@@ -3,7 +3,6 @@ export BUILD_HARNESS_PROJECT=${2:-build-harness}
 export BUILD_HARNESS_BRANCH=${3:-circle-2.0}
 export GITHUB_REPO="git@github.com:sagansystems/${BUILD_HARNESS_PROJECT}.git"
 
-cd ~
 if [ "$BUILD_HARNESS_PROJECT" ] && [ -d "$BUILD_HARNESS_PROJECT" ]; then
 	echo "Removing existing $BUILD_HARNESS_PROJECT"
   rm -rf "$BUILD_HARNESS_PROJECT"
