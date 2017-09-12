45ccbc9b93d36627ab7297eda72a2ca2efb444c6
Daren Desjardins <darend@gmail.com>
2017-09-12 10:06:41 -0700

    Install build-harness to pwd

--- bin/circleci.sh
+++ bin/circleci.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-export DOCKER_VERSION=${1:-1.9.1}
+
 export BUILD_HARNESS_PROJECT=${2:-build-harness}
 export BUILD_HARNESS_BRANCH=${3:-master}
 export GITHUB_REPO="git@github.com:sagansystems/${BUILD_HARNESS_PROJECT}.git"
 
-cd ~
 if [ "$BUILD_HARNESS_PROJECT" ] && [ -d "$BUILD_HARNESS_PROJECT" ]; then
 	echo "Removing existing $BUILD_HARNESS_PROJECT"
   rm -rf "$BUILD_HARNESS_PROJECT"
